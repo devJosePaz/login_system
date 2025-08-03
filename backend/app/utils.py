4fd7acac4e50c7296ccaae45b42f4cb2f137fc66
devJosePaz <lusjos40@gmail.com>
2025-08-03 19:44:11 -0300

    feat: adicao funcao create_access_token

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+from backend.settings import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +10,15 @@ def hash_password(password: str) -> str:
 
 def verify_password(password: str, hashed: str) -> bool:
     return pwd_context.verify(password, hashed)
+
+def create_access_token(data: dict, expires_data: timedelta = timedelta(hours=1)) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_data
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.JWT_SECRET,
+        algorithm=settings.JWT_ALGORITHM
+    )
+    return encoded_jwt
