f83fd5ad478b6ab7276b1b44174fd3aa6d9b8858
devJosePaz <lusjos40@gmail.com>
2025-08-03 19:45:18 -0300

    feat: adicao de atributo ACCESS_TOKEN_EXPIRE_MINUTES

--- backend/settings.py
+++ backend/settings.py
@@ -1,12 +1,13 @@
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
+from pathlib import Path
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
-    JWT_ALGORITHM: str
+    JWT_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"  
+        env_file = Path(__file__).resolve().parent.parent / ".env"  # equivale a ../.env
 
 settings = Settings()
