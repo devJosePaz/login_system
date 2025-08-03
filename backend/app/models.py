3bda258bb22c75df7293dae8f38a2b784a2f220e
devJosePaz <lusjos40@gmail.com>
2025-08-03 19:25:04 -0300

    fix: ajustado duplicacao de primary key

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,8 +5,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 class UserModel(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, primary_key=True, index=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     email: Mapped[str] = mapped_column(String(50), nullable=False, unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
-
-
