b3c4b70575b25a76f45dc50fa37d18ae17b05033
devJosePaz <lusjos40@gmail.com>
2025-08-03 19:50:48 -0300

    feat: adicao endpoint login

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -15,12 +15,38 @@ async def register(user_data: schemas.BaseModel, db: AsyncSession = Depends(get_
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="error: user already exists.")
 
     new_user = models.UserModel(email=user_data.email, password=utils.hash_password(user_data.password))
+
     
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
 
     return new_user
+
+@router.post("/login", response_model=schemas.UserResponse, status_code=status.HTTP_202_ACCEPTED)
+async def login(user_data: schemas.UserCreatem, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(models.UserModel).where(models.UserModel.email == user_data.email))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="error: user not found.")
+
+    if not utils.verify_password(user_data.password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="error: incorrect password.")
+
+    token = utils.create_access_token(data={"sub": user.email})
+    return {"access token": token, "token_type": "bearer"}
+
+
+
+
+    
+
+    
+    
+
+
+        
 
 
 
