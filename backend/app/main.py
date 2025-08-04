376021032df87c335b19eda183514055fcd4c16e
devJosePaz <lusjos40@gmail.com>
2025-08-04 11:42:38 -0300

    feat: funcao para criar as tabelas adicionado no main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
 from routes import router as auth_router
 from fastapi import FastAPI
+from app.database import Base
 
 app = FastAPI()
 
 app.include_router(auth_router.router)
+
+async def on_startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
