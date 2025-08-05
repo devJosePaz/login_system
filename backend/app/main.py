25dff34a3b6d052d8637d41caeec22898f362386
devJosePaz <lusjos40@gmail.com>
2025-08-04 21:24:23 -0300

    feat: adicao do middleware no main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
 from backend.app.routes import router 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.app.database import Base, engine
 
 app = FastAPI()
 
 app.include_router(router)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # permite GET, POST, PUT, DELETE, etc
+    allow_headers=["*"],  # permite headers Authorization e outros
+)
+
 
 @app.on_event("startup")
 async def on_startup():
